327b7a29394d5e2b523e4d0f85dad0631d1dcd13
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-09-25 18:43:56 -0400

    commit tree b36d61362fd170807040060d9351d595c1fb96fa

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase()
+}
+
+var bestCustomer = undefined;
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'dan';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'paul'
+}
